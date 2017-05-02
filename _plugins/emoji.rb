70005125c5368c47eb3198a156e6925ee93a9526
Stefan Haslinger <stefan.haslinger@mittenin.at>
2017-05-02 17:31:25 +0200

    talk, continued

--- _plugins/emoji.rb
+++ _plugins/emoji.rb
@@ -5,11 +5,9 @@ module Jekyll
     #
     #   {{ "1f474" | emoji }} => "audio/mp4a-latm"
     def emoji(character)
-      out =       %Q{<span style="width: 3em; height: 3em;">}
-      out = out + %Q{  <svg style="padding: 0; margin: 0; width:100%; height:100%;">}
+      out =       %Q{  <svg style="display: inline; padding: 0; margin: 0; width:100%; height:2em;">}
       out = out + %Q{    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#emoji-#{character}"></use>}
       out = out + %Q{  </svg>}
-      out = out + %Q{</span>}
     end
   end
 end
